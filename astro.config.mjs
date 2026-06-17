b1f057755aac38806081f43d49df01d22951b5aa
NohraManoel <manoel.nohra@gmail.com>
2026-06-17 16:50:18 -0400

    Hopefully the build is fixed

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,6 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
     site: 'https://manoel-nohra.fr',
-    base : '/',
     trailingSlash : 'ignore',
     i18n: {
     defaultLocale: 'en',
